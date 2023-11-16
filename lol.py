f75d08cf12959379a247ae7b6c1b3c7b98f98aa5
CrackerWithSalt <chepikovluka@gmail.com>
2023-11-16 19:47:31 +0200

    dddddddddddddddddddd

--- lol.py
+++ lol.py
@@ -1,12 +1,12 @@
-import main.py as main
-dataset1 = main.df2[df2['Market location'] == 'Central']
-dataset2 = main.df2[df2['Market location'] == 'East']
-dataset3 = main.df2[df2['Market location'] == 'South']
-dataset4 = main.df2[df2['Market location'] == 'West']
-main.fig, main.axes = plt.subplots(nrows=2, ncols=2, figsize=(13, 10))
+import main as main
+dataset1 = main.df2[main.df2['Market location'] == 'Central']
+dataset2 = main.df2[main.df2['Market location'] == 'East']
+dataset3 = main.df2[main.df2['Market location'] == 'South']
+dataset4 = main.df2[main.df2['Market location'] == 'West']
+main.fig, main.axes = main.plt.subplots(nrows=2, ncols=2, figsize=(13, 10))
 datasets = [dataset1, dataset2, dataset3, dataset4]
 labels = ['Central', 'East', 'South', 'West']
-for i, ax in enumerate(axes.flatten()):
+for i, ax in enumerate(main.axes.flatten()):
  data = datasets[i]
  ax.scatter(data['Sales'], data['Budget Sales'], label=labels[i])
  ax.set_title(labels[i])
