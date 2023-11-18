8a86b1e7e35b67183d765d5c2f2bcb83c7df3071
CrackerWithSalt <chepikovluka@gmail.com>
2023-11-18 21:00:04 +0200

    еееееееееееееее

---
+++ graph4heatmap.py
@@ -0,0 +1,8 @@
+import main
+import time as t #для маніпуляцій з датами
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.heatmap(main.df2.corr(numeric_only = True), cmap="YlGnBu", annot=True)#візуалізація кореляції
+plt.xticks(rotation=90)
+plt.show()
